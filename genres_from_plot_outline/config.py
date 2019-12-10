704c9821a3423520c0d742ddb38729118d86b2ee
James Smith <james99991smith@gmail.com>
2019-12-10 20:51:37 +0000

    Running select few models

--- genres_from_plot_outline/config.py
+++ genres_from_plot_outline/config.py
@@ -1,3 +1,4 @@
 movie_data_loc_1 = r"C:\Users\User\Documents\ITB Year 2\Text Analytics and Web Content Mining\Assignments\Assignment 2\Data\ml-20m\ml-20m\links.csv"
 movie_data_loc_2 = r"C:\Users\User\Documents\ITB Year 2\Text Analytics and Web Content Mining\Assignments\Assignment 2\Data\ml-latest-small\ml-latest-small\links.csv"
-dataset_loc = r"C:\Users\User\Documents\ITB Year 2\Text Analytics and Web Content Mining\Assignments\Assignment 2\Data\overview_dataset.csv"
+dataset_loc = r"C:\Users\User\Documents\ITB Year 2\Text Analytics and Web Content Mining\Assignments\Assignment 2\versioned_code\Genres-from-Plot-Outline\Data\overview_dataset.csv"
+dataset_post_init_exploration = r"C:\Users\User\Documents\ITB Year 2\Text Analytics and Web Content Mining\Assignments\Assignment 2\versioned_code\Genres-from-Plot-Outline\Data\dataset_after_init_exploration.csv"
--- notebooks/README.md
+++ notebooks/README.md
@@ -14,3 +14,12 @@ Notebook used to collect the plot overviews and genres for several thousand IMDB
 - Stop word removal
 - Address nan issue
 - Text similarity
+- Word cloud given the genre or combination!
+  - https://medium.com/towards-artificial-intelligence/multi-label-text-classification-using-scikit-multilearn-case-study-with-stackoverflow-questions-768cb487ad12
+- How many comments have multilabels https://towardsdatascience.com/multi-label-text-classification-with-scikit-learn-30714b7819c5
+
+### Modelling
+- Try with n-grams
+- Could use title to help with genres also
+- Easiest example - https://www.kaggle.com/roccoli/multi-label-classification-with-sklearn
+- https://www.kaggle.com/adamschroeder/countvectorizer-tfidfvectorizer-predict-comments GREAT KERNEL
